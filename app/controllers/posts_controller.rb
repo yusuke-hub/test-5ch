b7a4abd7c6462676e74900a7e32cb0c9babec4ca
yusuke-hub <yk1210140408@gmail.com>
2020-03-28 02:57:04 +0000

    Postコントローラーにアクションを定義

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.save!
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+  private
+    def post_params
+      params.require(:post).permit(:content)
+    end
+end
