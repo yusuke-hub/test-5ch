6156ba3e3db911ce3e7005842508aced592056a5
yusuke-hub <yk1210140408@gmail.com>
2020-03-28 08:25:39 +0000

    ビューファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'boards#index'
   resources :posts, only:[:show, :new, :create, :destroy]
-  resources :boards, only:[:index, :show, :new, :create, :destroy]
+  resources :boards, only:[:show, :new, :create, :destroy]
   resources :categories, only[:create, :destroy]
 end
