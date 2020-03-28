4aea76f9c7fb602c3b7d835e668091fd2dd5d795
yusuke-hub <yk1210140408@gmail.com>
2020-03-28 10:53:52 +0000

    Userテーブルにnameカラムを再追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_102925) do
+ActiveRecord::Schema.define(version: 2020_03_28_104812) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
