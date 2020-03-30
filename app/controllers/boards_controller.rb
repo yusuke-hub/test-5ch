7406391166d2947906ab6951279fb1db37472015
yusuke-hub <yk1210140408@gmail.com>
2020-03-30 01:06:02 +0000

    スレッド詳細画面を作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,9 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+  def show
+    @board = Board.find(params[:id])
+  end
   def new
     @board = Board.new
   end
