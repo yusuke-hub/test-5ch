7f093e5ddf2690facbaa0983dc1f596bd317f564
yusuke-hub <yk1210140408@gmail.com>
2020-03-30 11:18:50 +0000

    Userテーブルにadminカラムを追加/adminのUserコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :users
+  end
   root :to => 'boards#index'
   resources :posts, only:[:show, :new, :create, :destroy]
   resources :boards, only:[:show, :new, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_092925) do
+ActiveRecord::Schema.define(version: 2020_03_30_110934) do
 
   create_table "boards", force: :cascade do |t|
     t.string "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_092925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", default: "名無しさん", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
