3da29ef136292ff7dc4e545e340935f079c9c004
yusuke-hub <yk1210140408@gmail.com>
2020-03-30 11:35:36 +0000

    アクション(new,create)とストロングパラメーターを設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,21 @@
 class Admin::UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@usr.name}さん」を登録しました"
+    else
+      render :new
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :admin, :password)
+    end
+  end
 end
