1bd37523da089515a692f9acbb75b4abd628c435
yusuke-hub <yk1210140408@gmail.com>
2020-03-31 04:53:38 +0000

    アクションを追加(admin/boards.controller.rb)

--- app/controllers/admin/boards_controller.rb
+++ app/controllers/admin/boards_controller.rb
@@ -1,2 +1,10 @@
 class Admin::BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+  def show
+    @board = Board.find(params[:id])
+    @post = Post.new
+    @posts = Post.all
+  end
 end
