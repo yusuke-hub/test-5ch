553e08c620a672f56ff17e6df61121fbbc5bc1c0
yusuke-hub <yk1210140408@gmail.com>
2020-03-31 06:58:36 +0000

    ユーザー側コントローラーを再作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def new 
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    @user.save!
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :encrypted_password)
+    end
+end
