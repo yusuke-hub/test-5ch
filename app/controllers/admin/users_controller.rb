f09ff673877f299798e693f6dff266c54f61d7b6
yusuke-hub <yk1210140408@gmail.com>
2020-03-31 01:18:31 +0000

    管理者側のユーザ一覧を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
@@ -7,15 +10,23 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{@usr.name}さん」を登録しました"
+      redirect_to boards_path, notice: "ユーザー「#{@user.name}さん」を登録しました"
     else
       render :new
     end
+  end
 
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :admin, :password)
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, notice: "ユーザー「#{@user.name}さん」を削除しました"
     end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password)
   end
 end
