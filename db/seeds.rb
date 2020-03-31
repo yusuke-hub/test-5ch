d33dabecdc4cfaf715b62dedf2853419818b9c24
yusuke-hub <yk1210140408@gmail.com>
2020-03-31 09:50:08 +0000

    Seedファイルにadminのレコードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  [
+    {
+      email: 'admin@admin.com',
+      password: 'adminadmin'
+    }
+  ]
+)
 User.create!(
   [
     {
