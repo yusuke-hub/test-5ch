991999fada8067de3488a24b727105f924c3e909
yusuke-hub <yk1210140408@gmail.com>
2020-04-02 13:02:14 +0000

    カテゴリ管理画面にフラッシュメッセージを表示

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,15 +3,15 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
     @categories = Category.all
   end
-def create 
+  def create 
     @category = Category.new(category_params)
     @category.save!
-    redirect_to admin_categories_path
+    redirect_to admin_categories_path, flash: {notice: 'カテゴリを作成しました'}
   end
 	def toggle_status
 		@category = Category.find(params[:category_id])
-		 @category.update(status: @category.toggle_status)
-		redirect_to admin_categories_path
+		@category.update(status: @category.toggle_status)
+		redirect_to admin_categories_path, flash: {notice: 'ステータスを変更しました'}
 	end  
   private
     def category_params
